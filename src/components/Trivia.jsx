ff02c574083951301cc21007f2daab2b31b008cc
Kirill Goshin <4kirillgo@gmail.com>
2024-04-13 16:55:43 +0300

    created react app. Added initial texts and styles

---
+++ src/components/Trivia.jsx
@@ -0,0 +1,17 @@
+export default function Trivia() {
+    return (
+        <>
+            <div className="trivia">
+                <div className="question">
+                    What's the best youtube channel?
+                </div>
+                <div className="answers">
+                    <div className="answer">Kirill</div>
+                    <div className="answer">Kirill</div>
+                    <div className="answer">Kirill</div>
+                    <div className="answer">Kirill</div>
+                </div>
+            </div>
+        </>
+    )
+}
